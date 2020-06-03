9e47a756adf9686e21be0abaae808d75279c2343
Patrick Marcos <patrick.marcos.2013@gmail.com>
2020-06-03 12:12:58 -0300

    Atividade07/Exercicio2

---
+++ Atividades/Atividades07/ler_escrever_linhas.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(void)
+{
+FILE *arq;
+char nome_e_matricula[40];
+//char matricula[10];
+
+if((arq = fopen("C:\\Users\\dinib\\Desktop\\atividades\\ativi7\\Aluno2.txt", "w+"))== NULL)
+{
+perror("Descricao do erro:");
+exit(1);
+}
+while(1)
+{
+printf("Digite o Nome e matricula || ou sair(para sair)||Aperte espaco para nome e matricula:\n");
+	fgets(nome_e_matricula, 40, stdin);
+	nome_e_matricula[strlen(nome_e_matricula) - 1] = '\0';	
+if(strcmp("sair", nome_e_matricula))
+{
+//fprintf(arq, "%s\n", nome_e_matricula);
+fprintf(arq, "[%s]\n", nome_e_matricula);
+}
+else{	
+break;
+}}
+fclose(arq);
+
+return 0;
+}
